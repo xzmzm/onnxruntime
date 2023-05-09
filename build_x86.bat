d1fdc88c5111f0b9af6d0684f4b8c4275638c568
hm <hmchong@elsoftresearch.com>
2023-05-09 20:16:33 +0800

    win7_x64

--- build_x86.bat
+++ build_x86.bat
@@ -13,5 +13,5 @@ REM #endif
 
 
 REM SET CMAKE_BUILD_PARALLEL_LEVEL=3
-build --build_dir build\windows_x86a --config Release --cmake_generator "Visual Studio 17 2022" --x86 --cmake_extra_defines CMAKE_SYSTEM_VERSION:STRING=10.0 --cmake_extra_defines CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION:STRING=10.0 --build_shared_lib --enable_msvc_static_runtime --msvc_toolset 14.35.32215 --skip_tests --skip_submodule_sync --parallel 3
+build --build_dir build\windows_x86 --config Release --cmake_generator "Visual Studio 17 2022" --x86 --cmake_extra_defines CMAKE_SYSTEM_VERSION:STRING=10.0 --cmake_extra_defines CMAKE_VS_WINDOWS_TARGET_PLATFORM_VERSION:STRING=10.0 --build_shared_lib --enable_msvc_static_runtime --msvc_toolset 14.35.32215 --skip_tests --skip_submodule_sync --parallel 3
 REM --skip_submodule_sync
